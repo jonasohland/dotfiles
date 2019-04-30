3ddbdd274f5753af3e064c2f5064402379201cac
Jonas Ohland <jonas.ohland@gmail.com>
2019-04-30 23:50:20 +0200

    get_cat function

--- fish_functions/get_cat.fish
+++ fish_functions/get_cat.fish
@@ -1,5 +1,5 @@
 function get_cat
-	set new_cat_url (curl -sL https://api.thecatapi.com/v1/images/search | jq -r .[0].url)
-curl -sLO $new_cat_url
-open (string split "/" $new_cat_url)[-1]
+    set new_cat_url (curl -sL https://api.thecatapi.com/v1/images/search | jq -r .[0].url)
+    curl -sLO $new_cat_url
+    open (string split "/" $new_cat_url)[-1]
 end
