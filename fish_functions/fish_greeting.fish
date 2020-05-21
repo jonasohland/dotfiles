3788af371559ac1b8925b8daf82ffddc2d360539
Jonas Ohland <jonas.ohland@gmail.com>
2020-05-21 15:20:14 +0100

    make greeting and fish config more platform agnostic

--- fish_functions/fish_greeting.fish
+++ fish_functions/fish_greeting.fish
@@ -2,6 +2,8 @@ function fish_greeting
 	set_color green
     echo -e "\nWelcome back, Master Jonas\n"
     set_color normal
-    current_tasks
-    current_tracking
+    if type -q task
+        current_tasks
+        current_tracking
+    end
 end
