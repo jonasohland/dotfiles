3ae59f0e8fef4252b28f784e0bd244cbe0380106
Jonas Ohland <jonas.ohland@gmail.com>
2020-05-29 00:17:15 +0200

    modify ffplay whitelist

--- fish_functions/ffplay.fish
+++ fish_functions/ffplay.fish
@@ -1,3 +1,3 @@
 function ffplay
-command ffplay -hide_banner -protocol_whitelist file,sap,sdp,udp,tcp,rtsp,rtp $argv
+command ffplay -hide_banner -protocol_whitelist file,sap,sdp,udp,tcp,rtsp,rtp,http,https $argv
 end
